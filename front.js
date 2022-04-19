5a6036322deb391539e7e0c7aac19bfe74307052
JessieCheng <hamy820326@gmail.com>
2022-04-19 18:02:14 +0800

    feat: add access token rules - blank or expired
    blank: doesn't have token in web storage
    expired : when get 401 token expired at the first request (after getting token, then exec request again)

--- front.js
+++ front.js
@@ -17,20 +17,24 @@
 // }
 
 
-
-
 const ApiHelper = {
+  email: 'hamy820326@gmail.com',
+  password: '12345678',
   instance: null,
   init () {
+    console.log('axios init')
     this.instance = axios.create({
       baseURL: 'http://dataapimanage:8888'
     });
   },
-  getAccessToken (email, password) {
+  getAccessToken () {
+    console.log('getAccessToken')
     return new Promise((resolve, reject) => {
-      return this.instance.post('/login', { // arrow function 沒有自己的 this，會指向外層函式作用域的 this
-        email,
-        password
+      // arrow function 沒有自己的 this，會指向外層函式作用域的 this
+      // TODO: 解構復職
+      return this.instance.post('/login', {
+        email: this.email,
+        password: this.password
       })
         .then(response => {
           console.log(response.status)
@@ -42,6 +46,7 @@ const ApiHelper = {
 
             // Alter defaults after instance has been created
             this.instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+            console.log('set token to axios header 1')
 
             resolve(true)
           }
@@ -49,12 +54,54 @@ const ApiHelper = {
         .catch(err => reject(err))
     })
   },
-  getTestData () {
-    return this.instance.post('/test')
-      .then(({ data }) => {
-        return data
-      })
-      .catch(err => console.log(err.response))
+  async checkToken () {
+    console.log('check token')
+
+    if (!localStorage.getItem('token')) await this.getAccessToken()
+
+    // Alter defaults after instance has been created
+    this.instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+    console.log('set token to axios header 2')
+
+    // TODO: move to other place!!
+    // Add a response interceptor
+    this.instance.interceptors.response.use(function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    }, async (error) => {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      console.log(error.response)
+      if (error.response.status === 401 && error.response.data.message === 'The token expired.') {
+        try {
+          await this.getAccessToken()
+          error = 'already re-get access token'
+          console.log(`set error message: ${error}`)
+        } catch (err) {
+          error = err
+        }
+      }
+
+      return Promise.reject(error);
+    });
+  },
+  async getData (subPath) {
+    // TODO: 應該不用 async/await 了
+    const request = async () => {
+      console.log('get data request')
+      return await this.instance.post(`/${subPath}`)
+        .then(({ data }) => {
+          return data
+        })
+        .catch(err => {
+          return err
+        })
+    }
+    await this.checkToken() // FIXME: 這裡要加 await...
+    const result = await request()
+    if (result === 'already re-get access token') return await request()
+    else return result
   }
 }
 
@@ -62,18 +109,14 @@ const ApiHelper = {
 // ApiHelper.getAccessToken(email, password)
 
 
-const email = 'hamy820326@gmail.com'
-const password = '12345678';
 
 const frontView = {
-  async init (email, password) {
+  async init () {
     try {
-      ApiHelper.init()
+      if (!ApiHelper.instance) ApiHelper.init()
 
-      if (await ApiHelper.getAccessToken(email, password)) {
-        const result = await ApiHelper.getTestData()
-        console.log(result)
-      }
+      const result = await ApiHelper.getData('test')
+      console.log(result)
     } catch (err) {
       console.log(err)
     }
@@ -81,7 +124,7 @@ const frontView = {
 }
 
 // 沒穿帳密則 Server return 404 好像不是 fail json???
-frontView.init(email, password)
+frontView.init()
 
 
   // (async (email, password) => {
